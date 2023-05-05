3d72e0e585e54f9857d41dc95a43eb21f4146a76
TiboMertens <tibomertens25@gmail.com>
2023-05-06 01:53:00 +0200

    feat: send temp to ttn

--- lora_ttn.cpp
+++ lora_ttn.cpp
@@ -1,26 +1,55 @@
 #include "DHT.h"
+#include <TheThingsNetwork.h>
+
 #define DHTPIN 5
 #define DHTTYPE DHT11
 
+// Set your AppEUI and AppKey
+const char *appEui = "XXXXXXXXXXXXXXXX";
+const char *appKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+#define loraSerial Serial1
+#define debugSerial Serial
+
+#define freqPlan TTN_FP_EU868
+
+int LDR_Pin = A0; // Analog pin connected to LDR
+int LDR_Reading;  // Variable to store the LDR reading
+
 DHT dht(DHTPIN, DHTTYPE);
+TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin(); // initialize the sensor
+  debugSerial.begin(9600);
+  loraSerial.begin(57600);
+  dht.begin();
+  ttn.join(appEui, appKey);
 }
 
 void loop() {
-  // wait a few seconds between measurements.
+  // Wait a few seconds between measurements.
   delay(2000);
-  
-  // read temperature as Celsius
+
+  // Read temperature as Celsius.
   float tempC = dht.readTemperature();
 
-  // check if any reads failed
+  // Check if any reads failed.
   if (isnan(tempC)) {
-    Serial.println("Failed to read from DHT sensor!");
+    debugSerial.println("Failed to read from DHT sensor!");
   } else {
-    Serial.print(tempC);
-    Serial.println("°C");
+    debugSerial.print("Temperature: ");
+    debugSerial.print(tempC);
+    debugSerial.println("°C");
   }
+
+  // Prepare byte array to send temperature and LDR data.
+  byte data[2];
+  int16_t temp = (int16_t)(tempC * 100); // Convert float to int16_t.
+  data[0] = temp >> 8; // MSB of temperature.
+  data[1] = temp & 0xFF; // LSB of temperature.
+
+  // Send temperature and LDR data to TTN.
+  ttn.sendBytes(data, sizeof(data));
+
+  delay(10000); // Wait 10 seconds before sending again.
 }
