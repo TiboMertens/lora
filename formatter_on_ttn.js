f2885f5fcf81e949692e43ba1d9405fac668684b
TiboMertens <tibomertens25@gmail.com>
2023-05-06 16:49:06 +0200

    style: formatted code

--- formatter_on_ttn.js
+++ formatter_on_ttn.js
@@ -1,16 +1,16 @@
 function Decoder(bytes, port) {
-    // Decode an uplink message from a buffer
-    // (array) of bytes to an object of fields.
-    var decoded = {};
-          
-    if (port === 1) {
-      var tempC = ((bytes[0] << 8) | bytes[1]) / 100.0;
-      decoded.temperature = tempC.toFixed(2);
-      
-      // Extract the mapped LDR value from the payload
-      var ldr = bytes[2];
-      decoded.ldr = ldr;
-    }
-          
-    return decoded;
+  // Decode an uplink message from a buffer
+  // (array) of bytes to an object of fields.
+  var decoded = {};
+
+  if (port === 1) {
+    var tempC = ((bytes[0] << 8) | bytes[1]) / 100.0;
+    decoded.temperature = tempC.toFixed(2);
+
+    // Extract the mapped LDR value from the payload
+    var ldr = bytes[2];
+    decoded.ldr = ldr;
   }
+
+  return decoded;
+}
